b30c3fc31e13afbd9f3b4bd4b7e749f936968de7
Ryo3504 <megafan7@gmail.com>
2021-01-10 19:21:45 +0700

    Add filterConfig and webapiConfig

---
+++ SimpleBlog/BlogLearning/BlogLearning/App_Start/FilterConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace BlogLearning.App_Start
+{
+    public class Filteronfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
